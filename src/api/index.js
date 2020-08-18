4114996df76119dcdf1779b63727022eda815a01
smpark <smpark7723@gmail.com>
2020-08-19 07:27:38 +0900

    nodejs init commit

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+import Router from "koa-router";
+const api = new Router();
+export default api;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
+import api from './api';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+router.use(api.routes());
+
+app.use(cors());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+
+const port = process.env.PORT || 8090;
+export const server = app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+})
+
